6788a673b8b7d6eca6db19c322ad63022bda86e7
Mmesoma David <108488301+Mmesomadavid@users.noreply.github.com>
2026-03-09 12:03:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Portfolio/", // ← THIS IS CRUCIAL FOR GH PAGES
+  base: "/", // ← THIS IS CRUCIAL FOR GH PAGES
   plugins: [react()],
 });
